341a5f857a9c6e0dbbb0bc5c7f7c60d1c215309c
digitalninjanv <147027984+digitalninjanv@users.noreply.github.com>
2025-08-31 20:17:52 +0700

    Update welcome.js

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -15,7 +15,7 @@ export const welcome = () => {
         return `
             <img src="${coupleImage}" alt="couple animation">
             <figcaption>
-                ${brideLName.split(' ')[0]} & ${bridePName.split(' ')[0]}
+                ${brideLName.split(' ')[2]} & ${bridePName.split(' ')[0]}
             </figcaption>`;
     };
 
